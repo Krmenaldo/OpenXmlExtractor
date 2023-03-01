cb9458e4aaa42ac960d0ba135326d9344417cb60
Nikola Mihailović <nikola.mihailovic@intellya.ai>
2023-03-01 12:30:16 +0100

    init

---
+++ Paragraphs.cs
@@ -0,0 +1,27 @@
+﻿namespace ParagraphExtractor
+{
+    public class ExtractedParagraph
+    {
+        public string Heading1 { get; set; }
+        public string Heading2 { get; set; }
+        public string Heading3 { get; set; }
+        public string Paragraph { get; set; }
+
+        public ExtractedParagraph(string heading1, string heading2, string heading3, string paragraph)
+        {
+            Heading1 = heading1;
+            Heading2 = heading2;
+            Heading3 = heading3;
+            Paragraph = paragraph;
+        }
+        public override string ToString()
+        {
+            return Heading1 + "|" + Heading2 + "|" + Heading3 + "|" + Paragraph;
+        }
+    }
+
+    public class ExtractedDocument
+    {
+        public List<ExtractedParagraph> Paragraphs { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using ParagraphExtractor;
+
+Console.WriteLine("Hello, World!");
+string[] files =  {
+    "XXX"
+};
+foreach (var file in files)
+{
+    using (var doc = WordprocessingDocument.Open("documents\\" + file + ".docx", false))
+    {
+        var document = new ExtractedDocument
+        {
+            Paragraphs = new List<ExtractedParagraph>()
+        };
+        var heading1 = string.Empty;
+        var heading2 = string.Empty;
+        var heading3 = string.Empty;
+        var heading4 = string.Empty;
+        var heading5 = string.Empty;
+        var heading6 = string.Empty;
+        var level0counter = 0;
+        var level1counter = 0;
+        var level2counter = 0;
+        foreach (var item in doc.MainDocumentPart.Document.Body.ChildElements)
+        {
+            if (item is not Paragraph && item is not Table)
+                continue;
+            if (item is Table)
+            {
+                var table = (Table)item;
+                var lastRow = table.ChildElements.OfType<TableRow>().LastOrDefault();
+                var numOfColumns = 0;
+                if (lastRow != null)
+                    numOfColumns = lastRow.ChildElements.OfType<TableCell>().Count();
+
+                if (numOfColumns > 0)
+                {
+                    var tableHeader = table.ChildElements.OfType<TableRow>().FirstOrDefault(t => t.ChildElements.OfType<TableCell>().Count() == numOfColumns);
+                    if (tableHeader != null)
+                    {
+                        var tableText = string.Empty;
+                        foreach (var row in table.ChildElements.OfType<TableRow>().Where(t => t.ChildElements.OfType<TableCell>().Count() == numOfColumns).Skip(1))
+                        {
+                            var counter = 0;
+                            var rowName = string.Empty;
+                            foreach (var cell in row.ChildElements.OfType<TableCell>())
+                            {
+                                if (counter == 0)
+                                {
+                                    rowName = cell.InnerText;
+                                }
+                                else
+                                    tableText += "[" + rowName + ":" + tableHeader.ChildElements.OfType<TableCell>().ElementAt(counter)?.InnerText + "] " + cell.InnerText + " ";
+                                counter++;
+                            }
+                        }
+                        document.Paragraphs.Add(new ExtractedParagraph(heading1, heading2, FillAdditionalHeading(heading3, heading4, heading5, heading6), tableText));
+                    }
+                }
+
+            }
+            if (item is Paragraph)
+            {
+                var paragraph = (Paragraph)item;
+                var images = GetImagesFromParagraph(paragraph, doc);
+                if(images != null && images.Any())
+                {
+                    foreach (var image in images)
+                    {
+                        using (var fileStream = File.Create(Guid.NewGuid() + ExtensionFromContentType(image.ContentType)))
+                        {
+                            image.GetStream().CopyTo(fileStream);
+                        }
+                    }
+                }
+                var style = paragraph.ParagraphProperties?.ParagraphStyleId?.Val?.Value;
+                if ((!string.IsNullOrEmpty(style) && style.Contains("Heading")) || paragraph.ParagraphProperties?.NumberingProperties != null)
+                {
+                    if (!string.IsNullOrEmpty(style) && style.Contains("Heading1"))
+                    {
+                        heading1 = paragraph.InnerText;
+                        heading2 = string.Empty;
+                        heading3 = string.Empty;
+                        heading4 = string.Empty;
+                        heading5 = string.Empty;
+                        heading6 = string.Empty;
+                        level0counter = 0;
+                    }
+                    else if (!string.IsNullOrEmpty(style) && style.Contains("Heading2"))
+                    {
+                        heading2 = paragraph.InnerText;
+                        heading3 = string.Empty;
+                        heading4 = string.Empty;
+                        heading5 = string.Empty;
+                        heading6 = string.Empty;
+                        level0counter = 0;
+                    }
+                    else if (!string.IsNullOrEmpty(style) && style.Contains("Heading3"))
+                    {
+                        heading3 = paragraph.InnerText;
+                        heading4 = string.Empty;
+                        heading5 = string.Empty;
+                        heading6 = string.Empty;
+                        level0counter = 0;
+                    }
+                    else if (!string.IsNullOrEmpty(style) && style.Contains("Heading"))
+                    {
+                        if (style.Contains("Heading4"))
+                        {
+                            heading4 = paragraph.InnerText;
+                            heading5 = string.Empty;
+                            heading6 = string.Empty;
+                        }
+                        if (style.Contains("Heading5"))
+                        {
+                            heading5 = paragraph.InnerText;
+                            heading6 = string.Empty;
+                        }
+                        if (style.Contains("Heading6"))
+                            heading6 = paragraph.InnerText;
+                        level0counter = 0;
+                    }
+                    else if (paragraph.ParagraphProperties?.NumberingProperties != null)
+                    {
+                        var last = document.Paragraphs.LastOrDefault();
+                        if (last == null || last.Heading1 != heading1 || last.Heading2 != heading2 || last.Heading3 != heading3)
+                        {
+                            document.Paragraphs.Add(new ExtractedParagraph(heading1, heading2, FillAdditionalHeading(heading3, heading4, heading5, heading6), string.Empty));
+                            last = document.Paragraphs.LastOrDefault();
+                        }
+                        if (paragraph.ParagraphProperties.NumberingProperties.NumberingLevelReference.Val.Value == 0)
+                        {
+                            level0counter++;
+                            level1counter = 0;
+                            level2counter = 0;
+                            last.Paragraph += " \\n " + "[" + level0counter + "] " + paragraph.InnerText;
+                        }
+                        if (paragraph.ParagraphProperties.NumberingProperties.NumberingLevelReference.Val.Value == 1)
+                        {
+                            level1counter++;
+                            level2counter = 0;
+                            last.Paragraph += " \\n " + "[" + level0counter + "." + level1counter + "] " + paragraph.InnerText;
+                        }
+                        if (paragraph.ParagraphProperties.NumberingProperties.NumberingLevelReference.Val.Value == 2)
+                        {
+                            level2counter++;
+                            last.Paragraph += " \\n " + "[" + level0counter + "." + level1counter + "." + level2counter + "] " + paragraph.InnerText;
+                        }
+                    }
+                }
+                else
+                {
+                    document.Paragraphs.Add(new ExtractedParagraph(heading1, heading2, FillAdditionalHeading(heading3, heading4, heading5, heading6), paragraph.InnerText));
+                    level0counter = 0;
+                }
+            }
+        }
+        var text = string.Join(Environment.NewLine, document.Paragraphs.Select(p => p.ToString()));
+        File.WriteAllTextAsync(file + ".txt", text);
+    }
+}
+Console.ReadLine();
+
+string FillAdditionalHeading(string heading3, string heading4, string heading5, string heading6)
+{
+    if (!string.IsNullOrEmpty(heading4))
+        heading3 += ";" + heading4;
+    if (!string.IsNullOrEmpty(heading5))
+        heading3 += ";" + heading5;
+    if (!string.IsNullOrEmpty(heading6))
+        heading3 += ";" + heading6;
+    return heading3;
+}
+
+IEnumerable<ImagePart> GetImagesFromParagraph(Paragraph paragraph, WordprocessingDocument doc)
+{
+    var images = from graphic in paragraph
+                    .Descendants<DocumentFormat.OpenXml.Drawing.Graphic>()
+                 let graphicData = graphic.Descendants<DocumentFormat.OpenXml.Drawing.GraphicData>().FirstOrDefault()
+                 let pic = graphicData.ElementAt(0)
+                 let nvPicPrt = pic.ElementAt(0).FirstOrDefault()
+                 let blip = pic.Descendants<DocumentFormat.OpenXml.Drawing.Blip>().FirstOrDefault()
+                 join part in doc.MainDocumentPart.Parts on blip.Embed.Value equals part
+                     .RelationshipId
+                 let image = part.OpenXmlPart as ImagePart
+                 select image;
+    return images;
+}
+
+string ExtensionFromContentType(string contentType)
+{
+    switch(contentType)
+    {
+        case "image/png":
+            return ".png";
+        case "image/jpeg":
+        case "image/jpg":
+            return ".jpg";
+    }
+    return string.Empty;
+}
